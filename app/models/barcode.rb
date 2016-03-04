8a7a0df3d72d61c71618f0dfaab7c68dcab2c7f3
lum <ibmt20@hotmail.com>
2016-03-04 15:33:10 +0800

    printer

--- app/models/barcode.rb
+++ app/models/barcode.rb
@@ -36,6 +36,35 @@ class Barcode < ActiveRecord::Base
       ^LL0768
       ^LS0
       ^FT154,276^BQN,2,7
+      ^FDLA,#{hash[:id]}^FS
+      ^FT695,345^A0B,33,33^FH\^FDDate^FS
+      ^FT773,344^A0B,75,74^FH\^FD#{hash[:date]}^FS
+      ^FT270,763^A0B,33,33^FH\^FDDate Code^FS
+      ^FT359,763^A0B,75,74^FH\^FD#{hash[:date_code]}^FS
+      ^FT414,763^A0B,33,33^FH\^FDLot No^FS
+      ^FT496,763^A0B,75,74^FH\^FD#{hash[:lot_no]}^FS
+      ^FT151,763^A0B,33,33^FH\^FDMO^FS
+      ^FT694,763^A0B,33,33^FH\^FDQuantity^FS
+      ^FT217,763^A0B,75,74^FH\^FD#{hash[:mo]}^FS
+      ^FT557,763^A0B,33,33^FH\^FDLEI Product No^FS
+      ^FT772,763^A0B,75,74^FH\^FD#{hash[:qty]}^FS
+      ^FT644,763^A0B,75,74^FH\^FD#{hash[:product_no]}^FS
+      ^FT100,763^A0B,75,74^FH\^FDLeader Electronics Inc^FS
+      ^BY1,3,94^FT909,674^B3B,N,,Y,N
+      ^FD#{hash[:id]}^FS
+      ^PQ1,0,1,Y^XZ
+    "
+  end
+
+  def self.finish_goods_label_old(hash)
+    "
+      ^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR4,4~SD20^JUS^LRN^CI0^XZ
+      ^XA
+      ^MMT
+      ^PW945
+      ^LL0768
+      ^LS0
+      ^FT154,276^BQN,2,7
       ^FDLA,http://bc.l-e-i.com:3088/bc/#{hash[:id]}^FS
       ^FT695,345^A0B,33,33^FH\^FDDate^FS
       ^FT773,344^A0B,75,74^FH\^FD#{hash[:date]}^FS
